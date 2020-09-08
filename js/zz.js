b0631760763a1d9f233999c08b9695210cf2974a
David <david.javapagetemplates@gmail.com>
2020-09-08 02:15:38 +0200

    zz function improved: now it supports a selector id as: '#', '.', 't', 'tn', 'n' and 's'. zzs object removed.

--- js/zz.js
+++ js/zz.js
@@ -1,13 +1,42 @@
 /*
     zz function
-*/
-var zz = function( x ){
-    var inner;
     
+    zz( '#', 'id' );
+    zz( '.', 'className' );
+    zz( 't', 'tagName' );
+    zz( 'tn', 'namespace', 'tagName' );
+    zz( 'n', 'name' );
+    zz( 's', 'string selector' );
+    zz( 'string selector' );
+*/
+var zz = function( x, s1, s2 ){
+    
+    // Redefine x if a selector id is found
+    if ( s1 ){
+        switch ( x ){
+            case '#':
+                x = document.getElementById( s1 );
+            case '.':
+                x = document.getElementsByClassName( s1 );
+            case 't':
+                x = document.getElementsByTagName( s1 );
+            case 'tn':
+                x = document.getElementsByTagNameNS( s1, s2 );
+            case 'n':
+                x = document.getElementsByName( s1 );
+            case 's':
+                x = document.querySelector( s1 );
+            default:
+                throw 'Unsupported selector id found running zz function: ' + x;
+        }
+    }
+    
+    // x must be an Element, a NodeList or a standard string selector
     if ( x instanceof Element ){
         return new SimpleZZDom( x );
     }
-        
+    
+    var inner;
     if ( x instanceof NodeList ){
         inner = x;
         
@@ -19,29 +48,5 @@ var zz = function( x ){
         return inner.length === 1? new SimpleZZDom( inner[ 0 ] ): new MultipleZZDom( inner );
     }
     
-    throw 'Failed running zz function!';
-};
-
-/*
-    zz selector object
-*/
-var zzs = {
-    i: function( id ){
-        return document.getElementById( id );
-    },
-    c: function( className ){
-        return document.getElementsByClassName( className );
-    },
-    t: function( tagName ){
-        return document.getElementsByTagName( tagName );
-    },
-    tn: function( namespace, tagName ){
-        return document.getElementsByTagNameNS( namespace, tagName );
-    },
-    n: function( name ){
-        return document.getElementsByName( name );
-    },
-    s: function( string ){
-        return document.querySelector( string );
-    }
+    throw 'Unsupported selector type found running zz function.';
 };

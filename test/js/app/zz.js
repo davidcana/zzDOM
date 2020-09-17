63aececf3fd973d0699b4588cecfb1a7cc57d73c
David Cana <david.javapagetemplates@gmail.com>
2020-09-17 13:32:55 +0200

    Some more tests added to zz.js.

--- test/js/app/zz.js
+++ test/js/app/zz.js
@@ -135,4 +135,81 @@ QUnit.test( 'NodeList test', function( assert ) {
     assert.equal( zz( '#t4-1-3' ).text(), t4_1_modified );
 });
 
+QUnit.test( 'Nodes test', function( assert ) {
+    var t5_1_1_original = 'white',
+        t5_1_2_original = 'red',
+        t5_1_3_original = 'black',
+        t5_1_modified = 'yellow';
+    assert.equal( document.getElementById( 't5-1-1' ).textContent, t5_1_1_original );
+    assert.equal( document.getElementById( 't5-1-2' ).textContent, t5_1_2_original );
+    assert.equal( document.getElementById( 't5-1-3' ).textContent, t5_1_3_original );
+    var ids = [];
+    zz( document.querySelectorAll( '#t5-1 .t5-1' ) )
+        .text( t5_1_modified )
+        .each( function( zzEl ){ ids.push( zzEl.attr( 'id' ) ); } );
+    assert.deepEqual( ids, [ 't5-1-2', 't5-1-3' ] );
+    assert.equal( document.getElementById( 't5-1-1' ).textContent, t5_1_1_original );
+    assert.equal( document.getElementById( 't5-1-2' ).textContent, t5_1_modified );
+    assert.equal( document.getElementById( 't5-1-3' ).textContent, t5_1_modified );
+    assert.equal( zz( '#t5-1-1' ).text(), t5_1_1_original );
+    assert.equal( zz( '#t5-1-2' ).text(), t5_1_modified );
+    assert.equal( zz( '#t5-1-3' ).text(), t5_1_modified );
+});
 
+QUnit.test( 'Standard selectors test', function( assert ) {
+    var t6_1_1_original = 'white',
+        t6_1_2_original = 'red',
+        t6_1_3_original = 'black',
+        t6_1_modified = 'yellow';
+    assert.equal( document.getElementById( 't6-1-1' ).textContent, t6_1_1_original );
+    assert.equal( document.getElementById( 't6-1-2' ).textContent, t6_1_2_original );
+    assert.equal( document.getElementById( 't6-1-3' ).textContent, t6_1_3_original );
+    var ids = [];
+    zz( document.querySelectorAll( '#t6-1 .t6-1' ) )
+        .text( t6_1_modified )
+        .each( function( zzEl ){ ids.push( zzEl.attr( 'id' ) ); } );
+    assert.deepEqual( ids, [ 't6-1-2', 't6-1-3' ] );
+    assert.equal( document.getElementById( 't6-1-1' ).textContent, t6_1_1_original );
+    assert.equal( document.getElementById( 't6-1-2' ).textContent, t6_1_modified );
+    assert.equal( document.getElementById( 't6-1-3' ).textContent, t6_1_modified );
+    assert.equal( zz( '#t6-1-1' ).text(), t6_1_1_original );
+    assert.equal( zz( '#t6-1-2' ).text(), t6_1_modified );
+    assert.equal( zz( '#t6-1-3' ).text(), t6_1_modified );
+});
+
+QUnit.test( 'HTML code test', function( assert ) {
+    var t7_1_original = 'This is the container t7-1',
+        t7_1_modified = `
+This is the container t7-1
+
+<div id="t7-1-in">
+  New div
+</div>
+`;
+    utils.assertHtml( assert, 't7-1', t7_1_original );
+    var id = zz( '<div id="t7-1-in">New div</div>' )
+        .appendTo( '#t7-1' )
+        .attr( 'id' );
+    utils.assertHtml( assert, 't7-1', t7_1_modified );
+    assert.equal( id, 't7-1-in' );
+    
+    var t7_2_original = 'This is the container t7-2',
+        t7_2_modified = `
+This is the container t7-2
+
+<div id="t7-2-in1">
+  New div 1
+</div>
+
+<div id="t7-2-in2">
+  New div 2
+</div>
+`;
+    utils.assertHtml( assert, 't7-2', t7_2_original );
+    var ids = [];
+    zz( '<div id="t7-2-in1">New div 1</div><div id="t7-2-in2">New div 2</div>' )
+        .appendTo( '#t7-2' )
+        .each( function( zzEl ){ ids.push( zzEl.attr( 'id' ) ); } );
+    assert.deepEqual( ids, [ 't7-2-in1', 't7-2-in2' ] );
+    utils.assertHtml( assert, 't7-2', t7_2_modified );
+});

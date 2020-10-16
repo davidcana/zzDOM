b4ddd48c20c0d7c65313fe419c6d444d2acfe0f2
David Cana <david.javapagetemplates@gmail.com>
2020-10-16 13:06:07 +0200

    zzDOM._paramItem and zzDOM.param moved from ss.js to zzDOM.js. Tests added.

--- build/zzDOM-gcc.js
+++ build/zzDOM-gcc.js
@@ -1,4 +1,4 @@
-/*! zzdom - v0.2.0 - 2020-10-16 12:39:29 */
+/*! zzdom - v0.2.0 - 2020-10-16 13:3:43 */
 /**
  * A namespace.
  * @const
@@ -192,6 +192,41 @@ zzDOM._getDefaultDisplay = function( el ) {
     return display;
 };
 /* End of default display */
+
+/* Utils */
+// Serialize a ss instance, a mm instance or an object into a query string
+zzDOM._paramItem = function( r, key, value ) {
+    r.push( 
+        encodeURIComponent( key ) + '=' + encodeURIComponent( value == null? '': value )
+    );
+};
+/** @nocollapse */
+zzDOM.param = function( x ) {
+	
+    if ( x == null ) {
+        return '';
+    }
+
+    var r = [];
+    
+    if ( x instanceof zzDOM.SS ){
+        zzDOM._paramItem( r, x.attr( 'name' ), x.val() );
+    } else if ( x instanceof zzDOM.MM ){
+        for ( var c = 0; c < x.list.length; ++c ){
+            var ss = x.list[ c ];
+            zzDOM._paramItem( r, ss.attr( 'name' ), ss.val() );
+        }
+    } else if ( typeof x === 'object' ){  
+        for ( var i in x ) {
+            zzDOM._paramItem( r, i, x[ i ] );
+        }
+    } else {
+        throw zzDOM._getError( 'param' );
+    }
+
+    return r.join( '&' );
+};
+/* end of utils */
 
 // Register zz function
 var zz;
@@ -852,44 +887,6 @@ zzDOM.SS.prototype.val = function ( value ) {
     return this;
 };
 /* End of forms */
-
-/* Utils */
-
-zzDOM._paramItem = function( r, key, value ) {
-    r.push( 
-        encodeURIComponent( key ) + '=' + encodeURIComponent( value == null? '': value )
-    );
-};
-
-//TODO test this
-// Serialize a ss instance, a mm instance or an object into a query string
-/** @nocollapse */
-zzDOM.param = function( x ) {
-	
-    if ( x == null ) {
-        return '';
-    }
-
-    var r = [];
-    
-    if ( x instanceof zzDOM.SS ){
-        zzDOM._paramItem( r, x.attr( 'name' ), x.val() );
-    } else if ( x instanceof zzDOM.MM ){
-        for ( var c = 0; c < x.list.length; ++c ){
-            var ss = x.list[ c ];
-            zzDOM._paramItem( r, ss.attr( 'name' ), ss.val() );
-        }
-    } else if ( typeof x === 'object' ){  
-        for ( var i in x ) {
-            zzDOM._paramItem( r, i, x[ i ] );
-        }
-    } else {
-        throw zzDOM._getError( 'param' );
-    }
-
-    return r.join( '&' );
-};
-/* end of utils */
 
 /* Center */
 //TODO implement this
--- src/zzDOM.js
+++ src/zzDOM.js
@@ -193,6 +193,41 @@ zzDOM._getDefaultDisplay = function( el ) {
 };
 /* End of default display */
 
+/* Utils */
+// Serialize a ss instance, a mm instance or an object into a query string
+zzDOM._paramItem = function( r, key, value ) {
+    r.push( 
+        encodeURIComponent( key ) + '=' + encodeURIComponent( value == null? '': value )
+    );
+};
+/** @nocollapse */
+zzDOM.param = function( x ) {
+	
+    if ( x == null ) {
+        return '';
+    }
+
+    var r = [];
+    
+    if ( x instanceof zzDOM.SS ){
+        zzDOM._paramItem( r, x.attr( 'name' ), x.val() );
+    } else if ( x instanceof zzDOM.MM ){
+        for ( var c = 0; c < x.list.length; ++c ){
+            var ss = x.list[ c ];
+            zzDOM._paramItem( r, ss.attr( 'name' ), ss.val() );
+        }
+    } else if ( typeof x === 'object' ){  
+        for ( var i in x ) {
+            zzDOM._paramItem( r, i, x[ i ] );
+        }
+    } else {
+        throw zzDOM._getError( 'param' );
+    }
+
+    return r.join( '&' );
+};
+/* end of utils */
+
 // Register zz function
 var zz;
 (function() { 

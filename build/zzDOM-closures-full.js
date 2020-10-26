5a9c5360c01af0693dfe002f2e226ef00d5bc165
David Cana <david.javapagetemplates@gmail.com>
2020-10-26 12:31:42 +0100

    Bug fixed: code about events removed from zzDOM.js, it is already in zzDOM-events.js.

--- build/zzDOM-closures-full.js
+++ build/zzDOM-closures-full.js
@@ -1,4 +1,4 @@
-/*! zzdom - v0.2.0 - 2020-10-26 10:47:6 */
+/*! zzdom - v0.2.0 - 2020-10-26 12:28:29 */
 /**
  * A namespace.
  * @const
@@ -109,70 +109,6 @@ var zz;
 (function() { 
     zz = zzDOM.zz; 
 })();
-
-//TODO create events plugin
-/* Events */
-zzDOM._events = {};
-
-zzDOM._addEventListener = function( ss, eventName, listener, useCapture ){
-    var el = ss.el;
-    var elId = ss._getElId();
-    var thisEvents = zzDOM._events[ elId ];
-    if ( ! thisEvents ){
-        thisEvents = {};
-        zzDOM._events[ elId ] = thisEvents;
-    }
-    var thisListeners = thisEvents[ eventName ];
-    if ( ! thisListeners ){
-        thisListeners = [];
-        thisEvents[ eventName ] = thisListeners;
-    }
-    thisListeners.push( listener );
-    
-    // addEventListener
-    el.addEventListener( eventName, listener, useCapture );
-};
-
-//TODO must remove all listeners when an element is removed
-zzDOM._removeEventListener = function( ss, eventName, listener, useCapture ){
-    var el = ss.el;
-    var elId = ss._getElId();
-    var thisEvents = zzDOM._events[ elId ];
-    if ( ! thisEvents ){
-        return;
-    }
-    
-    if ( ! eventName ){ 
-        // Must remove all events
-        for ( var currentEventName in thisEvents ){
-            var currentListeners = thisEvents[ currentEventName ];
-            zzDOM._removeListeners( el, currentListeners, null, useCapture, currentEventName );
-        }
-        return;
-    }
-    
-    // Must remove listeners of only one event
-    var thisListeners = thisEvents[ eventName ];
-    zzDOM._removeListeners( el, thisListeners, listener, useCapture, eventName );
-};
-
-//TODO test all the listeners are removed
-zzDOM._removeListeners = function( el, thisListeners, listener, useCapture, eventName ){
-    if ( ! thisListeners ){
-        return;
-    }
-    for ( var i = 0; i < thisListeners.length; ++i ){
-        var currentListener = thisListeners[ i ];
-        if ( ! listener || currentListener === listener ){
-            thisListeners.splice( i, 1 ); // Delete listener at i position
-            el.removeEventListener( eventName, currentListener, useCapture );
-            if ( listener ){
-                return;
-            }
-        }
-    } 
-};
-/* End of events */
 
 zzDOM._events = {};
 

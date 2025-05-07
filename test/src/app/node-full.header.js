1e923dae08f006f9d873527d1a3aafab6fbfc22a
David Cana <david.javapagetemplates@gmail.com>
2025-05-07 09:18:55 +0200

    Useless file deleted: node-zz.header.js. All commented references to that file deleted.

--- test/src/app/node-full.header.js
+++ test/src/app/node-full.header.js
@@ -1,7 +1,6 @@
 "use strict";
 
 var Qunit = require( 'qunit' );
-//var utils = require( '../test/src/app/utils-node.js' );
 
 var zzDOM = require( './zzDOM-closures-full.js' );
 var zz = zzDOM.zz;
--- test/src/app/node-plugin-center.header.js
+++ test/src/app/node-plugin-center.header.js
@@ -1,7 +1,6 @@
 "use strict";
 
 var Qunit = require( 'qunit' );
-//var utils = require( '../test/src/app/utils-node.js' );
 
 var zz = require( '../index.js' );
 require( '../plugin-center.js' );
